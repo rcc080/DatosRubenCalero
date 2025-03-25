e387f13579084d1300ca7bd2babc575941020933
rcc080 <rcc080@educa.madrid.org>
2025-03-25 20:59:06 +0100

    V2. Añadida edad

--- src/datos/Main.java
+++ src/datos/Main.java
@@ -1,5 +1,10 @@
 package datos;
 
 public class Main {
+	public static void main(String[] args) {
+		System.out.println("Nombre: Ruben");
+		System.out.println("Apellido: Calero");
+		System.out.println("Edad: 27");
+	}
 
 }
